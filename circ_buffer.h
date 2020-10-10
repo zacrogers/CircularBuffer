213932b5e3d9b60dd4f5be2bee16cd3f2bb40618
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-10-10 15:10:03 +1300

    Create circ_buffer.h

---
+++ circ_buffer.h
@@ -0,0 +1,29 @@
+#ifndef _CIRC_BUFFER_H
+#define _CIRC_BUFFER_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+struct circular_buffer
+{
+	uint16_t* buffer;
+	size_t head;
+	size_t tail;
+	size_t max;
+	bool full;
+};
+
+typedef circular_buffer* cbuff_handle;
+
+cbuff_handle cbuff_init(uint16_t* buffer, size_t size);
+bool         cbuff_full(cbuff_handle circ_buff);
+bool         cbuff_empty(cbuff_handle circ_buff);
+void         cbuff_put(cbuff_handle circ_buff, uint16_t data);
+void         cbuff_get(cbuff_handle circ_buff);
+size_t       cbuff_size(cbuff_handle circ_buff);
+void         cbuff_reset(cbuff_handle circ_buff);
+void         cbuff_free(cbuff_handle circ_buff);
+
+
+#endif /* _CIRC_BUFFER_H */
