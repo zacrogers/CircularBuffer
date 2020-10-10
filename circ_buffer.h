7229a514aa552b6006ebb51bd57ddec25d20e1c4
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-10-10 15:10:52 +1300

    Update circ_buffer.h

--- circ_buffer.h
+++ circ_buffer.h
@@ -20,7 +20,7 @@ cbuff_handle cbuff_init(uint16_t* buffer, size_t size);
 bool         cbuff_full(cbuff_handle circ_buff);
 bool         cbuff_empty(cbuff_handle circ_buff);
 void         cbuff_put(cbuff_handle circ_buff, uint16_t data);
-void         cbuff_get(cbuff_handle circ_buff);
+void         cbuff_get(cbuff_handle circ_buff, uint16_t* data);
 size_t       cbuff_size(cbuff_handle circ_buff);
 void         cbuff_reset(cbuff_handle circ_buff);
 void         cbuff_free(cbuff_handle circ_buff);
